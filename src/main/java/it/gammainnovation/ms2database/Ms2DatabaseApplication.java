689290f83133bd1a47ac4e89288f5cb4ce952bae
frabarbara <francesco.barbara5697@gmail.com>
2023-11-07 16:44:35 +0100

    init commit

---
+++ src/main/java/it/gammainnovation/ms2database/Ms2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.ms2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms2DatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms2DatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/gammainnovation/ms2database/Ms2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.ms2database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms2DatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
